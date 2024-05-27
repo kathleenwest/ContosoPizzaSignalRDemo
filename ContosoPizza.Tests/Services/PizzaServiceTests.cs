1a8a255eb584e8f7cf1df0f1759bf787cd43b91f
Kathleen West <kathleen.elizabeth.west@gmail.com>
2024-05-27 17:43:13 -0400

    remove temp projects

--- ContosoPizza.Tests/Services/PizzaServiceTests.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ContosoPizza.Tests.Services
-{
-    internal class PizzaServiceTests
-    {
-        // See PizzaControllerTests for Unit Test Demo
-        // Normally we should include unit tests for Service
-        // but this demo project is limited in scope
-    }
-}
