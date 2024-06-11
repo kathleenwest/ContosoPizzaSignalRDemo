7a1d3d055266e58fb3885c7d918943f57c3289bb
Kathleen West <kathleen.elizabeth.west@gmail.com>
2024-06-10 23:13:47 -0400

    disable form upon order submission

--- ContosoPizza/Models/Pizza.cs
+++ ContosoPizza/Models/Pizza.cs
@@ -5,15 +5,25 @@
     /// </summary>
     public class Pizza
     {
-        public Guid id { get; set; }
+        /// <summary>
+        /// Id
+        /// Globally unique identifier for the pizza
+        /// </summary>
+        public Guid Id { get; set; }
         
+        /// <summary>
+        /// Pizza Details: What the customer wants
+        /// </summary>
         public string PizzaDetails { get; set; }
 
+        /// <summary>
+        /// Construtor
+        /// When a new pizza is created, it is assigned a guid
+        /// </summary>
         public Pizza()
         {
-            id = Guid.NewGuid();
+            Id = Guid.NewGuid();
         }
 
     } // end of class
-
 } // end of namespace
