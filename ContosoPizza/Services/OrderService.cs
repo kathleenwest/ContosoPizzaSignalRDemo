c2de522c9e23e0beb24ed51f0566088ee44a7603
Kathleen West <kathleen.elizabeth.west@gmail.com>
2024-11-18 19:23:03 -0500

    add code comments and organization

--- ContosoPizza/Services/OrderService.cs
+++ ContosoPizza/Services/OrderService.cs
@@ -5,10 +5,37 @@ namespace ContosoPizza.Services
 {
     public interface IOrderService
     {
+        /// <summary>
+        /// Order a Pizza
+        /// </summary>
+        /// <param name="order">(PizzaOrder) pizza order</param>
+        /// <returns>a Task without result</returns>
         Task OrderPizzaAsync(PizzaOrder order);
+
+        /// <summary>
+        /// Returns all Pizza Orders
+        /// </summary>
+        /// <returns>List of (PizzaOrder) objects</returns>
         List<PizzaOrder> GetAllPizzaOrders();
+
+        /// <summary>
+        /// Get Pizza Order
+        /// </summary>
+        /// <param name="id">Guid of the known pizza order</param>
+        /// <returns>a (PizzaOrder) object (if existing)</returns>
         PizzaOrder? GetPizzaOrder(Guid id);
+
+        /// <summary>
+        /// Updates the Existing Pizza Order
+        /// </summary>
+        /// <param name="pizzaOrder">existing (PizzaOrder) order</param>
         void UpdatePizzaOrder(PizzaOrder pizzaOrder);
+
+        /// <summary>
+        /// Send Pizza Order Update to Customer
+        /// </summary>
+        /// <param name="order">(PizzaOrder) order</param>
+        /// <returns>a Task without results</returns>
         Task SendCustomerOrderStatusUpdateAsync(PizzaOrder order);
     }
 
@@ -20,42 +47,75 @@ namespace ContosoPizza.Services
         /// </summary>
         private static List<PizzaOrder> PizzaOrders { get; set; } = new List<PizzaOrder>();
 
+        /// <summary>
+        /// Reference to the IHubContext (SignalR)
+        /// </summary>
         private readonly IHubContext<OrdersHub> _hubContext;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="hubContext">IHubContext of type OrdersHub</param>
         public OrderService(IHubContext<OrdersHub> hubContext)
         {
             _hubContext = hubContext;
         }
 
+        /// <summary>
+        /// Order a Pizza
+        /// </summary>
+        /// <param name="order">(PizzaOrder) pizza order</param>
+        /// <returns>a Task without result</returns>
         public async Task OrderPizzaAsync(PizzaOrder order)
         {           
-            // Add to the Pizza Order
+            // Add to the Pizza Orders
             PizzaOrders.Add(order);
 
             // Update the Pizza Order Status
             order.Status = OrderStatus.Received;
 
-            // Send the admin with the new order
+            // Send the pizza store admin with the new order
             await _hubContext.Clients.All.SendAsync("ReceiveAdminOrderUpdate", "New Pizza Order From Customer: " + order.CustomerName);
         }
 
+        /// <summary>
+        /// Returns all Pizza Orders
+        /// </summary>
+        /// <returns>List of (PizzaOrder) objects</returns>
         public List<PizzaOrder> GetAllPizzaOrders()
         {
             return PizzaOrders;
         }
 
+        /// <summary>
+        /// Get Pizza Order
+        /// </summary>
+        /// <param name="id">Guid of the known pizza order</param>
+        /// <returns>a (PizzaOrder) object (if existing)</returns>
         public PizzaOrder? GetPizzaOrder(Guid id) => PizzaOrders.FirstOrDefault(p => p.OrderId == id);
 
+        /// <summary>
+        /// Updates the Existing Pizza Order
+        /// </summary>
+        /// <param name="pizzaOrder">existing (PizzaOrder) order</param>
         public void UpdatePizzaOrder(PizzaOrder pizzaOrder)
         {
+            // Find the existing pizza order
             int index = PizzaOrders.FindIndex(p => p.OrderId == pizzaOrder.OrderId);
 
+            // Pizza not found
             if (index == -1)
                 return;
 
+            // Update the Pizza order
             PizzaOrders[index] = pizzaOrder;
         }
 
+        /// <summary>
+        /// Send Pizza Order Update to Customer
+        /// </summary>
+        /// <param name="order">(PizzaOrder) order</param>
+        /// <returns>a Task without results</returns>
         public async Task SendCustomerOrderStatusUpdateAsync(PizzaOrder order)
         {
             if (order.Status != OrderStatus.New)
@@ -65,5 +125,5 @@ namespace ContosoPizza.Services
             }
         }
 
-    }
-}
+    } // end of class
+} // end of namespace
